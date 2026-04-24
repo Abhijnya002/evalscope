b1fbfc97444460fd5883584ef0f7ec2bbb9a1857
Yunnglin <mao.looper@qq.com>
2026-04-24 11:09:06 +0800

    update

---
+++ evalscope/api/model/__init__.py
@@ -0,0 +1,14 @@
+from .generate_config import GenerateConfig
+from .lazy_model import LazyModel
+from .model import Model, ModelAPI, get_model, get_model_with_task_config
+from .model_output import (
+    ChatCompletionChoice,
+    Logprob,
+    Logprobs,
+    ModelOutput,
+    ModelUsage,
+    StopReason,
+    TopLogprob,
+    as_stop_reason,
+)
+from .perf_metrics import PerformanceMetrics, calc_tpot
